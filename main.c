467e58e984b61efc8ce7b3f258de090329e35dd1
Urszula Kaminska <urkamins@c2r3s3.42warsaw.pl>
2026-08-09 11:52:42 +0200

    chore: norminette

--- main.c
+++ main.c
@@ -1,26 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: urkamins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/09 11:49:22 by urkamins          #+#    #+#             */
+/*   Updated: 2026/08/09 11:52:19 by urkamins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    double  disorder;
-    int     algorithm;
-    stack_t stack_a;
-    meta_t  sort_metdata;
+	double	disorder;
+	int		algorithm;
+	int		bench;
+	stack_t	*stack_a;
+	meta_t	*sort_metdata;
 
-    if argc > 1 
-    {
-        if (!validate_list(++argv))
-        {
-            print_err();
-            return 1;
-        }
-        populate_stack(argv, stack_a);
-        disorder = calc_disorder(stack_a);
-        algorithm = choose_algorithm(argv, disorder);
-        sort_stack(stack_a, sort_metadata, disorder, algorithm);
-        print_operations(sort_metadata->ops);
-        if (is_bench_flag(argv))
-            print_bench(sort_metadata);
-    }
+	if (argc > 1)
+	{
+		if (!validate_list(++argv))
+		{
+			print_err();
+			return (1);
+		}
+		stack_a = populate_stack(argv);
+		disorder = calc_disorder(stack_a);
+		algorithm = extract_flags(argv, disorder, *bench);
+		sort_stack(*stack_a, *sort_metadata, disorder, algorithm);
+		print_operations(sort_metadata->ops);
+		if (is_bench_flag(argv))
+			print_bench(sort_metadata);
+	}
+	return (0);
 }
