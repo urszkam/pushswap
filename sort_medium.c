3b7fc61e9fa6b70f468abf835946a7271d321470
Ula Kaminska <s28722@pjwstk.edu.pl>
2026-08-10 22:57:50 +0200

    init commit

---
+++ sort_medium.c
@@ -0,0 +1,7 @@
+#include "push_swap.h"
+
+void    sort_medium(t_stack **stack_a)
+{
+    ft_printf("simple\n");
+    (void) stack_a;
+}
---
+++ sort_simple.c
@@ -0,0 +1,7 @@
+#include "push_swap.h"
+
+void    sort_simple(t_stack **stack_a)
+{
+    ft_printf("simple\n");
+    (void) stack_a;
+}
