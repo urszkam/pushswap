a0602657383e5d83d3de4174eb8774be383c7339
Ula Kaminska <s28722@pjwstk.edu.pl>
2026-08-11 11:24:53 +0200

    feat: add op counter info

--- print_bench.c
+++ print_bench.c
@@ -42,11 +42,44 @@ void	print_algorithm(char *algorithm, double disorder)
 		ft_putendl_fd("O(n log n)", 2);
 }
 
-void	print_bench(double disorder, char *algorithm)
+void	print_total(t_meta meta)
+{
+	ft_putstr_fd("[bench] total_ops: ", 2);
+	ft_putnbr_fd(meta.total, 2);
+	ft_putendl_fd("", 2);
+}
+
+void	print_operations(t_meta meta)
+{
+	ft_putstr_fd("[bench] sa: ", 2);
+	ft_putnbr_fd(meta.sa, 2);
+	ft_putstr_fd(" sb: ", 2);
+	ft_putnbr_fd(meta.sb, 2);
+	ft_putstr_fd(" ss: ", 2);
+	ft_putnbr_fd(meta.ss, 2);
+	ft_putstr_fd(" pa: ", 2);
+	ft_putnbr_fd(meta.pa, 2);
+	ft_putstr_fd(" pb: ", 2);
+	ft_putnbr_fd(meta.pb, 2);
+	ft_putstr_fd("\n[bench] ra: ", 2);
+	ft_putnbr_fd(meta.ra, 2);
+	ft_putstr_fd(" rb: ", 2);
+	ft_putnbr_fd(meta.rb, 2);
+	ft_putstr_fd(" rr: ", 2);
+	ft_putnbr_fd(meta.rr, 2);
+	ft_putstr_fd(" rra: ", 2);
+	ft_putnbr_fd(meta.rra, 2);
+	ft_putstr_fd(" rrb: ", 2);
+	ft_putnbr_fd(meta.rrb, 2);
+	ft_putstr_fd(" rrr: ", 2);
+	ft_putnbr_fd(meta.rrr, 2);
+	ft_putendl_fd("", 2);
+}
+
+void	print_bench(double disorder, char *algorithm, t_meta meta)
 {
 	print_disorder(disorder);
 	print_algorithm(algorithm, disorder);
-	ft_putendl_fd("[bench] total_ops: [placeholder]", 2);
-	ft_putendl_fd("[bench] placeholder", 2);
-	ft_putendl_fd("[bench] placeholder", 2);
+	print_total(meta);
+	print_operations(meta);
 }
