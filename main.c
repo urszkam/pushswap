8bf297ff25d6bd3988d37df1892faf5df1df38e9
fun <internaut@tuta.io>
2026-08-22 08:12:09 +0200

    Validator corrected

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pausulzy <pausulzy@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/10 23:02:40 by urkamins          #+#    #+#             */
-/*   Updated: 2026/08/18 10:05:07 by pausulzy         ###   ########.fr       */
+/*   Updated: 2026/08/18 12:39:45 by pausulzy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,10 +79,8 @@ int	main(int argc, char **argv)
 	t_meta	meta;
 	t_stack	*stack_a;
 
-	if (argc == 1)
-		return (0);
 	ft_bzero(&meta, sizeof(t_meta));
-	if (!validate_list(argc, argv))
+	if (!validate_list(argc, ++argv))
 		return (1);
 	extract_flags(&argv, &algorithm_flag, &bench);
 	stack_a = populate_stack(argv);
--- validate_list.c
+++ validate_list.c
@@ -6,11 +6,49 @@
 /*   By: pausulzy <pausulzy@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/17 15:11:05 by pausulzy          #+#    #+#             */
-/*   Updated: 2026/08/17 15:26:26 by pausulzy         ###   ########.fr       */
+/*   Updated: 2026/08/22 08:06:02 by pausulzy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+// Filter flags
+int	is_flag(char *arg)
+{
+	if (arg[0] != '-' || arg[1] != '-')
+		return (0);
+	if ((ft_strncmp(arg, "--simple", 9) == 0 && arg[8] == '\0')
+		|| (ft_strncmp(arg, "--medium", 9) == 0 && arg[8] == '\0')
+		|| (ft_strncmp(arg, "--complex", 10) == 0 && arg[9] == '\0')
+		|| (ft_strncmp(arg, "--adaptive", 11) == 0 && arg[10] == '\0'))
+		return (1);
+	return (0);
+}
+
+// Return argv with flags filtered out
+static char	**filter_flags(char **argv)
+{
+	char	**filtered;
+	int		i;
+	int		count;
+
+	count = 0;
+	i = 1;
+	while (argv[i])
+		if (!is_flag(argv[i++]))
+			count++;
+	if (count == 0)
+		return (NULL);
+	filtered = ft_calloc(count + 1, sizeof(char *));
+	if (!filtered)
+		return (NULL);
+	i = 1;
+	count = 0;
+	while (argv[i])
+		if (!is_flag(argv[i]))
+			filtered[count++] = argv[i++];
+	return (filtered);
+}
 
 // Confirm all entries are digits or +/-
 static int	check_digits(char **argv)
@@ -18,9 +56,7 @@ static int	check_digits(char **argv)
 	int	i;
 	int	j;
 
-	if (!argv)
-		return (1);
-	i = 1;
+	i = 0;
 	while (argv[i])
 	{
 		j = 0;
@@ -29,11 +65,8 @@ static int	check_digits(char **argv)
 		if (!argv[i][j])
 			return (0);
 		while (argv[i][j])
-		{
-			if (!ft_isdigit(argv[i][j]))
+			if (!ft_isdigit(argv[i][j++]))
 				return (0);
-			j++;
-		}
 		i++;
 	}
 	return (1);
@@ -47,7 +80,7 @@ static int	check_range(char **argv)
 	int		sign;
 	long	value;
 
-	i = 1;
+	i = 0;
 	while (argv[i])
 	{
 		j = 0;
@@ -57,10 +90,13 @@ static int	check_range(char **argv)
 			if (argv[i][j++] == '-')
 				sign = -1;
 		while (argv[i][j])
-			value = value * 10 + (argv[i][j++] - '0');
-		value *= sign;
-		if (value < -2147483648 || value > 2147483647)
-			return (0);
+		{
+			if ((sign == -1 && value > (2147483648L - (argv[i][j] - '0')) / 10)
+				|| (value > (2147483647L - (argv[i][j] - '0')) / 10))
+				return (0);
+			else
+				value = value * 10 + (argv[i][j++] - '0');
+		}
 		i++;
 	}
 	return (1);
@@ -72,7 +108,7 @@ static int	check_repeats(char **argv)
 	int	i;
 	int	j;
 
-	i = 1;
+	i = 0;
 	while (argv[i])
 	{
 		j = i + 1;
@@ -87,13 +123,14 @@ static int	check_repeats(char **argv)
 // Validate list
 int	validate_list(int argc, char **argv)
 {
-	if (argc < 2 || !argv || !*argv)
+	char	**nums;
+
+	if (argc < 2)
+		return (0);
+	nums = filter_flags(argv);
+	if (!nums || !check_digits(nums) || !check_range(nums)
+		|| !check_repeats(nums))
 		return (ft_putendl_fd("Error", 2), 0);
-	if (!check_digits(argv))
-		return (ft_putendl_fd("Error", 2), 0);
-	if (!check_range(argv))
-		return (ft_putendl_fd("Error", 2), 0);
-	if (!check_repeats(argv))
-		return (ft_putendl_fd("Error", 2), 0);
+	free(nums);
 	return (1);
 }
