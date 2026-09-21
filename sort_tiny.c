d775eb9982bd9b5ea2d1ff83277df81f4936dcba
fun <internaut@tuta.io>
2026-09-21 17:24:33 +0200

    refactor: standardized to norm

--- sort_tiny.c
+++ sort_tiny.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sort_tiny.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: urkamins <urkamins@student.42warsaw.fr>    +#+  +:+       +#+        */
+/*   By: pausulzy <pausulzy@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/10 23:02:40 by urkamins          #+#    #+#             */
-/*   Updated: 2026/08/10 23:02:40 by urkamins         ###   ########.fr       */
+/*   Updated: 2026/09/21 17:23:30 by pausulzy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	sort_tiny(t_stack **stack, t_meta *meta)
 {
-    while (!is_sorted(*stack))
-    {
-        if (gt((*stack)->content, (*stack)->next->content))
-            sa(stack, meta);
-        if (gt((*stack)->content, ft_lstlast(*stack)->content))
-            rra(stack, meta);
-        else if (!is_sorted(*stack) && ft_lstsize(*stack) == 3)
-            ra(stack, meta);
-    }
+	while (!is_sorted(*stack))
+	{
+		if (gt((*stack)->content, (*stack)->next->content))
+			sa(stack, meta);
+		if (gt((*stack)->content, ft_lstlast(*stack)->content))
+			rra(stack, meta);
+		else if (!is_sorted(*stack) && ft_lstsize(*stack) == 3)
+			ra(stack, meta);
+	}
 }
